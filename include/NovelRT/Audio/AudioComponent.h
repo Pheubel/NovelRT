77bf13e178fce4d1c36d58f5e362497534766d75
Ken Johnson <k.rjohnson94@outlook.com>
2021-02-19 17:43:29 -0500

    Iteration 1: AudioComponent

---
+++ include/NovelRT/Audio/AudioComponent.h
@@ -0,0 +1,19 @@
+// Copyright © Matt Jones and Contributors. Licensed under the MIT Licence (MIT). See LICENCE.md in the repository root
+// for more information.
+#include <stdint.h>
+
+#ifndef NOVELRT_AUDIO_AUDIOSOUNDCOMPONENT_H
+#define NOVELRT_AUDIO_AUDIOSOUNDCOMPONENT_H
+
+namespace NovelRT::Audio
+{
+  struct AudioSoundComponent
+  {
+    public:
+      uintptr_t id;
+      float volume;
+      float pitch;
+  };
+}
+
+#endif //NOVELRT_AUDIO_AUDIOCOMPONENT_H
